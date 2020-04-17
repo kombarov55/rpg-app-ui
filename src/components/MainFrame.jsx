9e4ee0eb097f4ca9cbca95f699dfb03aec866ca2
n.kombarov <n.kombarov@pflb.ru>
2020-04-17 00:43:19 +0000

    есть первая вёрстка главного фрейма

--- src/components/MainFrame.jsx
+++ src/components/MainFrame.jsx
@@ -3,9 +3,32 @@ import React from "react"
 class MainFrame extends React.Component {
     render() {
         return (
-            <h1 className={"sample"}>
-                SUKA
-            </h1>
+            <div className={"main-vertical"}>
+                <div className={"head"}>
+                    <div className={"head-name"}>Название</div>
+                    <div className={"head-logo"}>Логотип</div>
+                </div>
+                <div className={"main-frame-body"}>
+                    <div className={"main-frame-nav"}>
+                        <div className={"main-frame-nav-item"}>Доска объявлений</div>
+                        <div className={"main-frame-nav-item"}>Мои объявления</div>
+                        <div className={"main-frame-nav-item"}>Избранное</div>
+                        <div className={"main-frame-nav-item"}>Сообщения</div>
+                        <div className={"main-frame-nav-item"}>Мои игры</div>
+                        <div className={"main-frame-nav-item"}>Кабинет</div>
+                        <div className={"main-frame-nav-item"}>Квесты</div>
+                        <div className={"main-frame-nav-item"}>Панель администратора</div>
+                    </div>
+                    <div className={"main-frame-view"}>
+                        <div className={"main-frame-view-title"}>Доска объявлений</div>
+                        <span className={"button"}>Подать объявление</span>
+                    </div>
+                </div>
+                <div className={"main-frame-footer"}>
+                    <div className={"footer-copyright"}>Копирайты</div>
+                    <div className={"footer-copyright"}>Реклама текстовой строкой</div>
+                </div>
+            </div>
         );
     }
 }
