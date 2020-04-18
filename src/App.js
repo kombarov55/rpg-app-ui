4a9067e86363d76d07ef2ed0fda63835577df4f9
n.kombarov <n.kombarov@pflb.ru>
2020-04-18 13:38:04 +0000

    ха

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import "primeflex/primeflex.css"
 import MainFrame from "./components/MainFrame";
 
 function App() {
--- src/components/AnnouncementItem.jsx
+++ src/components/AnnouncementItem.jsx
@@ -1,18 +1,22 @@
 import React from "react"
+import {Panel} from "primereact/panel"
 
 class AnnouncementItem extends React.Component {
     render() {
         return (
-            <div className={"announcement-view-list-item"}>
-                <div className={"announcement-view-list-item-title"}>{this.props.title}</div>
-                <div className={"announcement-view-list-item-description"}>{this.props.description}</div>
-                <div className={"announcement-view-list-item-footer"}>
-                    <div className={"announcement-view-list-item-footer-item"}>В избранное</div>
-                    <div className={"announcement-view-list-item-footer-item"}>Откликнуться</div>
-                    <div className={"announcement-view-list-item-footer-item"}>Связаться с автором</div>
-                    <div className={"announcement-view-list-item-footer-item"}>Комментарии (0)</div>
-                </div>
-            </div>
+            <Panel header={this.props.title}>
+                {this.props.description}
+            </Panel>
+            // <div className={"announcement-view-list-item"}>
+            //     <div className={"announcement-view-list-item-title"}>{this.props.title}</div>
+            //     <div className={"announcement-view-list-item-description"}>{this.props.description}</div>
+            //     <div className={"announcement-view-list-item-footer"}>
+            //         <div className={"announcement-view-list-item-footer-item"}>В избранное</div>
+            //         <div className={"announcement-view-list-item-footer-item"}>Откликнуться</div>
+            //{        <div className={"announcement-view-list-item-footer-item"}>Связаться с автором</div>}
+            //{        <div className={"announcement-view-list-item-footer-item"}>Комментарии (0)</div>}
+            //{    </div>}
+            // </div>
         )
     }
 }
