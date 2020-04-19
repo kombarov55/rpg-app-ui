b372f524b353146008bde39a06a863045f67d24b
n.kombarov <n.kombarov@pflb.ru>
2020-04-19 02:24:39 +0000

    меню, но хуевое вышло

--- src/components/MainFrame.jsx
+++ src/components/MainFrame.jsx
@@ -1,7 +1,14 @@
 import React from "react"
 import Announcement from "./Announcement";
+import {Sidebar} from "primereact/sidebar";
+import {Button} from "primereact/button";
+
 
 class MainFrame extends React.Component {
+    state = {
+        visible: false
+    }
+
     render() {
         return (
             <div className={"main-vertical"}>
@@ -10,9 +17,19 @@ class MainFrame extends React.Component {
                     <div className={"head-logo"}>Логотип</div>
                 </div>
                 <div className={"main-frame-body"}>
+                    <Sidebar visible={this.state.visible} onHide={() => this.setState({visible: false})}>
+                        <div>Доска объявлений</div>
+                        <div>Мои объявления</div>
+                        <div>Избранное</div>
+                        <div>Сообщения</div>
+                        <div>Мои игры</div>
+                        <div>Кабинет</div>
+                        <div>Квесты</div>
+                        <div>Панель администратора</div>
+                    </Sidebar>
                     <div className={"main-frame-view"}>
                         <div className={"main-frame-view-head"}>
-                            <i className="pi pi-bars bars-icon" style={{"font-size": "5vmax"}}></i>
+                            <i className="pi pi-bars bars-icon" style={{"font-size": "5vmax"}} onClick={ () => this.setState({visible: true})}></i>
                             <span className={"main-frame-view-title"}>Доска объявлений</span>
                             <div></div>
                         </div>
