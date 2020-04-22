aba59140b49b9f3063a9147e22b3c5da6a1f6e71
n.kombarov <n.kombarov@pflb.ru>
2020-04-22 16:57:43 +0000

    добавил для devtools

--- src/data-layer/Store.js
+++ src/data-layer/Store.js
@@ -9,4 +9,4 @@ export const initialState = {
     announcementForm: {}
 }
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
