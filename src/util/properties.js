19702e2ff502aae26505117e26e77d0a04e0edc8
n.kombarov <n.kombarov@pflb.ru>
2020-04-25 00:54:49 +0000

    правки по стилям

--- src/util/properties.js
+++ src/util/properties.js
@@ -1,3 +1,3 @@
-export const rootUrl = "http://localhost:8080"
+export const rootUrl = "https://localhost:8080"
 export const announcementUrl = rootUrl + "/announcement"
 export const uploadUrl = rootUrl + "/upload"
