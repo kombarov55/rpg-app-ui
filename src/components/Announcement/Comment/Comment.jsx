5eb8e73da6963a02a4ba52ec93b8c0dace34c2f9
n.kombarov <n.kombarov@pflb.ru>
2020-04-29 20:18:09 +0000

    Добавил вёрстку комментов

---
+++ src/components/Announcement/Comment/Comment.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Comment(props) {
+
+    return (
+        <div className={"comment"}>
+            <img
+                className={"comment-author-image"}
+                src={props.authorImgSrc}/>
+            <div className={"comment-content"}>
+                <div className={"comment-author-name"}>{props.authorFullName}</div>
+                <div className={"comment-text"}>{props.text}</div>
+                <div className={"comment-creation-date"}>{props.creationDate}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Comment
--- src/components/Announcement/Comment/CommentSection.jsx
+++ src/components/Announcement/Comment/CommentSection.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import CommentsCreationForm from "./CommentsCreationForm";
+import Comment from "./Comment";
 
 class CommentSection extends React.Component {
     render() {
         return (
             <div className={"comment-section"}>
-                Comments...
+                <Comment
+                    id={"1"}
+                    authorImgSrc={"https://sun9-41.userapi.com/c850436/v850436953/9d901/_AF7xArtAcg.jpg"}
+                    authorFullName={"Николай Комбаров"}
+                    creationDate={new Date().toUTCString()}
+                    text={"Что то там про объявление. Очень очень длинное сообщение. оно должно быть длинным чтобы я мог увидеть как текст распределяется по контейнеру"}
+                />
+                <Comment
+                    id={"1"}
+                    authorImgSrc={"https://sun9-41.userapi.com/c850436/v850436953/9d901/_AF7xArtAcg.jpg"}
+                    authorFullName={"Николай Комбаров"}
+                    creationDate={new Date().toUTCString()}
+                    text={"Бла бла бла "}
+                />
+                <Comment
+                    id={"1"}
+                    authorImgSrc={"https://sun9-41.userapi.com/c850436/v850436953/9d901/_AF7xArtAcg.jpg"}
+                    authorFullName={"Николай Комбаров"}
+                    creationDate={new Date().toUTCString()}
+                    text={"Бла бла бла "}
+                />
                 <CommentsCreationForm announcementId={this.props.announcementId} />
             </div>
         )
