8c76f8d5c25a9777066508d782665b0619e6e470
n.kombarov <n.kombarov@pflb.ru>
2020-04-29 22:01:46 +0000

    удалил закоменченый код

--- src/components/Announcement/Comment/CommentSection.jsx
+++ src/components/Announcement/Comment/CommentSection.jsx
@@ -30,27 +30,6 @@ class CommentSection extends React.Component {
                         text={comment.text}
                     />
                 ))}
-                {/*<Comment*/}
-                {/*    id={"1"}*/}
-                {/*    authorImgSrc={"https://sun9-41.userapi.com/c850436/v850436953/9d901/_AF7xArtAcg.jpg"}*/}
-                {/*    authorFullName={"Николай Комбаров"}*/}
-                {/*    creationDate={new Date().toUTCString()}*/}
-                {/*    text={"Что то там про объявление. Очень очень длинное сообщение. оно должно быть длинным чтобы я мог увидеть как текст распределяется по контейнеру"}*/}
-                {/*/>*/}
-                {/*<Comment*/}
-                {/*    id={"1"}*/}
-                {/*    authorImgSrc={"https://sun9-41.userapi.com/c850436/v850436953/9d901/_AF7xArtAcg.jpg"}*/}
-                {/*    authorFullName={"Николай Комбаров"}*/}
-                {/*    creationDate={new Date().toUTCString()}*/}
-                {/*    text={"Бла бла бла "}*/}
-                {/*/>*/}
-                {/*<Comment*/}
-                {/*    id={"1"}*/}
-                {/*    authorImgSrc={"https://sun9-41.userapi.com/c850436/v850436953/9d901/_AF7xArtAcg.jpg"}*/}
-                {/*    authorFullName={"Николай Комбаров"}*/}
-                {/*    creationDate={new Date().toUTCString()}*/}
-                {/*    text={"Бла бла бла "}*/}
-                {/*/>*/}
                 <CommentsCreationForm announcementId={this.props.announcementId} />
             </div>
         )
