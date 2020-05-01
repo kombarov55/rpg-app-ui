e7a4b90ddfe6bebd8333314cfd9faca80de20b62
n.kombarov <n.kombarov@pflb.ru>
2020-05-01 13:17:00 +0000

    удалил лишнее

--- src/components/Dialog/View/ConversationsView.js
+++ src/components/Dialog/View/ConversationsView.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ConversationItem from "../ConversationItem";
-import FormatDate from "../../../util/FormatDate";
 import {connect} from "react-redux";
-import {get} from "../../../util/Http";
-import {conversationUrl} from "../../../util/Parameters";
-import Globals from "../../../util/Globals";
-import {addConversation} from "../../../data-layer/ActionCreators";
 
 function mapStateToProps(state, props) {
     return {
@@ -33,31 +28,6 @@ function ConversationsView(props) {
                     />
                 ))
             }
-
-            {/*<ConversationItem*/}
-            {/*    companionImgSrc={"https://sun9-39.userapi.com/c206624/v206624729/a57dc/HS1ds38r7rA.jpg?ava=1"}*/}
-            {/*    companionFullName={"Николай Комбаров"}*/}
-            {/*    msgDateFormatted={FormatDate(new Date())}*/}
-            {/*    text={"Как дела? но мне тут надо очень длинно написать чтобы понять что всё нормально грузится..."}*/}
-            {/*/>*/}
-            {/*<ConversationItem*/}
-            {/*    companionImgSrc={"https://sun9-39.userapi.com/c206624/v206624729/a57dc/HS1ds38r7rA.jpg?ava=1"}*/}
-            {/*    companionFullName={"Николай Комбаров"}*/}
-            {/*    msgDateFormatted={FormatDate(new Date())}*/}
-            {/*    text={"Как дела? но мне тут надо очень длинно написать чтобы понять что всё нормально грузится..."}*/}
-            {/*/>*/}
-            {/*<ConversationItem*/}
-            {/*    companionImgSrc={"https://sun9-39.userapi.com/c206624/v206624729/a57dc/HS1ds38r7rA.jpg?ava=1"}*/}
-            {/*    companionFullName={"Николай Комбаров"}*/}
-            {/*    msgDateFormatted={FormatDate(new Date())}*/}
-            {/*    text={"Как дела? но мне тут надо очень длинно написать чтобы понять что всё нормально грузится..."}*/}
-            {/*/>*/}
-            {/*<ConversationItem*/}
-            {/*    companionImgSrc={"https://sun9-39.userapi.com/c206624/v206624729/a57dc/HS1ds38r7rA.jpg?ava=1"}*/}
-            {/*    companionFullName={"Николай Комбаров"}*/}
-            {/*    msgDateFormatted={FormatDate(new Date())}*/}
-            {/*    text={"Как дела? но мне тут надо очень длинно написать чтобы понять что всё нормально грузится..."}*/}
-            {/*/>*/}
         </div>
     )
 }
