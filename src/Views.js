51c638a5abb865aa30ebbe156bf7ecdb61ccc5f7
n.kombarov <n.kombarov@pflb.ru>
2020-05-01 13:17:51 +0000

    изменил структуру пакетов

--- src/Views.js
+++ src/Views.js
@@ -4,7 +4,7 @@ import AnnoucementCreation from "./components/Announcement/AnnoucementCreation";
 import AnnouncementView from "./components/Announcement/View/AnnouncementView";
 import MyAnnouncementView from "./components/Announcement/View/MyAnnouncementView";
 import FavoriteAnnouncementView from "./components/Announcement/View/FavoriteAnnouncementView";
-import ConversationsView from "./components/Dialog/View/ConversationsView";
+import ConversationsView from "./components/Conversation/View/ConversationsView";
 
 export const announcementView = {component: <AnnouncementView/>, label: "Доска объявлений"}
 export const myAnnouncementView = {component: <MyAnnouncementView/>, label: "Мои объявления"}
