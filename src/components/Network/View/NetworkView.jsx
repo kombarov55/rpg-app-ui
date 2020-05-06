914e9513d35e72fff010f9821a524cc8fa1e71e4
n.kombarov <n.kombarov@pflb.ru>
2020-05-06 15:51:46 +0000

    добавил список игр

--- src/components/Network/View/NetworkView.jsx
+++ src/components/Network/View/NetworkView.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import NetworkItem from "../NetworkItem";
+import GameItem from "../GameItem";
 
 function mapStateToProps(state, props) {
     return {}
@@ -22,7 +23,9 @@ export default connect(mapStateToProps, mapDispatchToProps)(function (props) {
                 <div className={"network-description"}>
                     Здравствуй, дорогой друг.
                     Я немного объясню, что мы такое.
-                    Нам не важно, где ты работал раньше или работаешь сейчас. Здесь тебя примут вне зависимости от места жительства, семейного положения, ориентации, веры или расы. Здесь мы стараемся дать то, в поисках чего так часто люди блуждают в сети.
+                    Нам не важно, где ты работал раньше или работаешь сейчас. Здесь тебя примут вне зависимости от места
+                    жительства, семейного положения, ориентации, веры или расы. Здесь мы стараемся дать то, в поисках
+                    чего так часто люди блуждают в сети.
                     Это — твой дом.
                 </div>
             </div>
@@ -43,6 +46,27 @@ export default connect(mapStateToProps, mapDispatchToProps)(function (props) {
                 <NetworkItem
                     imgSrc={"https://sun9-70.userapi.com/c855032/v855032568/1a8a73/TN5VCGj9Vlc.jpg"}
                     title={"❖ COMICSDREAMS ❖"}
+                />
+            </div>
+
+            <div className={"games-label"}>
+                Игры:
+            </div>
+            <div className={"games-view-horizontal"}>
+                <GameItem imgSrc={"https://sun9-64.userapi.com/c858416/v858416297/1c6f50/HpIP0jOcov4.jpg"}
+                          title={"Звёздные Войны ❖ Ролевая Игра ❖ Star Wars"}
+                />
+                <GameItem imgSrc={"https://sun9-50.userapi.com/c206728/v206728029/e6b05/y0oZUh43Mp4.jpg"}
+                          title={"Гарри Поттер ❖ Ролевая игра ❖ Harry Potter"}
+                />
+                <GameItem imgSrc={"https://sun9-47.userapi.com/c206628/v206628029/e92bc/tiooZwDgav0.jpg"}
+                          title={"Шерлок ❖ Ролевая Игра ❖ Sherlock"}
+                />
+                <GameItem imgSrc={"https://sun9-47.userapi.com/c206628/v206628029/e92bc/tiooZwDgav0.jpg"}
+                          title={"Шерлок ❖ Ролевая Игра ❖ Sherlock"}
+                />
+                <GameItem imgSrc={"https://sun9-27.userapi.com/c857420/v857420029/1d203f/tKLlbcriafc.jpg"}
+                          title={"Ривердейл ❖ Ролевая Игра ❖ Сабрина"}
                 />
             </div>
         </div>
