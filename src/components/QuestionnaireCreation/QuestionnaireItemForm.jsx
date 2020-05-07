efbae39d138fe34be83a9c801f81bd604611cf1b
n.kombarov <n.kombarov@pflb.ru>
2020-05-07 00:57:27 +0000

    чуть поправил верстку создания пункта анкеты<

--- src/components/QuestionnaireCreation/QuestionnaireItemForm.jsx
+++ src/components/QuestionnaireCreation/QuestionnaireItemForm.jsx
@@ -5,37 +5,43 @@ import QuestionnaireInputList from "./QuestionnaireInputList";
 import Btn from "../Common/Btn";
 
 function mapStateToProps(state, props) {
-    return {
-    
-    }
+    return {}
 }
 
 function mapDispatchToProps(dispatch, props) {
-    return {
-    
-    }
+    return {}
 }
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(function (props) {
     return (
         <div className={"questionnaire-creation-item-form"}>
-            <input className={"questionnaire-item-name-input"}
-                   placeholder={"Название пункта"}/>
+            <div className={"questionnaire-creation-item-form-label"}>Название пункта:</div>
+            <input className={"questionnaire-item-name-input"}/>
+
+            <div className={"questionnaire-creation-item-form-label"}>Тип:</div>
             <SelectButton options={[
                 {label: "Текстовое", value: "string"},
                 {label: "Числовое", value: "numeric"},
                 {label: "Графическое ", value: "graphic"},
                 {label: "Выбор из списка", value: "list"}
             ]}/>
+            <div className={"questionnaire-creation-item-form-label"}>Элемент списка:</div>
             <div className={"questionnaire-input-horizontal"}>
-                <input className={"questionnaire-item-list-input"}
-                       placeholder={"Элемент списка"}/>
+                <input className={"questionnaire-item-list-input"}/>
                 <i className={"pi pi-plus-circle"}
                    style={{"fontSize": "5vmax"}}
                 />
             </div>
             <QuestionnaireInputList/>
+
+            <div className={"questionnaire-creation-item-form-disclaimer"}>
+                <div className={"questionnaire-creation-item-form-disclaimer-text"}>
+                    Будьте внимательны! Это отобразится в листе персонажа
+                </div>
+                <i className={"pi pi-exclamation-circle"}
+                   style={{"fontSize": "5vmax"}}/>
+            </div>
             <div className={"mobile-button save-questionnaire-button"}>
                 Сохранить
             </div>
--- src/components/QuestionnaireCreation/View/QuestionnaireCreationView.jsx
+++ src/components/QuestionnaireCreation/View/QuestionnaireCreationView.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import {connect} from "react-redux";
 import QuestionnaireItemForm from "../QuestionnaireItemForm";
+import QuestionnaireAddItemButton from "../QuestionnaireAddItemButton";
 import QuestionnaireItem from "../QuestionnaireItem";
-import SkillItem from "../SkillItem";
-import SkillpointsDistributionForm from "../SkillpointsDistributionForm";
-import SkillItemForm from "../SkillItemForm";
+
 
 function mapStateToProps(state, props) {
     return {}
@@ -19,13 +18,31 @@ export default connect(mapStateToProps, mapDispatchToProps)(function (props) {
 
     return (
         <div className={"questionnaire-creation-view"}>
-        {/*<SkillItemForm/>*/}
-            <SkillItem
-                name={"Сила"}
-                type={"Общий"}
-                imgSrc={"https://i.pinimg.com/originals/bb/6f/06/bb6f0636acac89991930340e6c4f91d2.jpg"}
-                description={"Strength is one of the three primary attributes with the theme of defending against magic damage. It grants health and health regeneration to every hero. Strength heroes also gain attack damage per point of strength."}
+            {/*<QuestionnaireItemForm/>*/}
+            <QuestionnaireItem name={"Имя персонажа"}
+                               type={"Текстовое"}
             />
+            <QuestionnaireItem name={"Мин. возраст персонажа"}
+                               type={"Текстовое"}
+            />
+
+            <QuestionnaireItem name={"Город"}
+                               type={"Выбор из списка"}
+                               listValues={["Москва", "Тула", "Воронеж", "Ростов"]}
+            />
+
+            <QuestionnaireItemForm/>
+
+            <QuestionnaireAddItemButton/>
+
+
+        {/*<SkillItemForm/>*/}
+        {/*    <SkillItem*/}
+        {/*        name={"Сила"}*/}
+        {/*        type={"Общий"}*/}
+        {/*        imgSrc={"https://i.pinimg.com/originals/bb/6f/06/bb6f0636acac89991930340e6c4f91d2.jpg"}*/}
+        {/*        description={"Strength is one of the three primary attributes with the theme of defending against magic damage. It grants health and health regeneration to every hero. Strength heroes also gain attack damage per point of strength."}*/}
+        {/*    />*/}
         </div>
     )
 })
